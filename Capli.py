5feb749316f7afc9f3c317602e4e17cdb9d79508
VasiliySoldatkin <34882906+VasiliySoldatkin@users.noreply.github.com>
2017-12-27 22:15:13 +0300

    Update Capli.py

--- Capli.py
+++ Capli.py
@@ -1,6 +1,7 @@
 a, b = map(int,input().split())
 maximum = min(a, b)
-if max(a,b)%2 == 1:
-     print(max(a,b)//2 +1, maximum)
+m = max(a,b)
+if m%2 == 1:
+     print(m//2 +1, maximum)
 else:
-     print(max(a,b)//2, maximum)
+     print(m//2, maximum)
