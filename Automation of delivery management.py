ce4aa758954194826755c2dc64aacf263627f3ad
VasiliySoldatkin <34882906+VasiliySoldatkin@users.noreply.github.com>
2018-01-23 14:59:25 +0300

    Update Automation of delivery management.py

--- Automation of delivery management.py
+++ Automation of delivery management.py
@@ -2,12 +2,12 @@ from math import ceil
 k = int(input())
 x = int(input())
 y = int(input())
-maxx = x + k - 1
-sec = ceil(y / x)*x
-if y%x == 0:
+maxx = x + k - 1 # Максимальный вес пакета
+sec = ceil(y / x)*x # Минимальный вес контейнера при минимальном весе пакета
+if y%x == 0: #Исключение варианта, при котором можно уложить в контейнер пакеты минимального веса, т.е. по x кг
      print(y)
      exit()
-if ceil(y / x) - 1 < ceil(y/maxx) :
+if ceil(y / x) - 1 < ceil(y/maxx) : 
      print(sec)
 else:
-     print(y)
+     print(y) # Случай, когда контейнер может состоять из пакетов максимального веса для получения min_weight 
